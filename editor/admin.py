956a733682bf043df448a355a119d8bea2f267de
Niki Bizjak <drobilc@gmail.com>
2020-07-28 17:53:43 +0200

    Add admin views for Document and Collection models

--- editor/admin.py
+++ editor/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'visibility', 'owner')
+    list_display_links = ('title', )
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'owner')
+    list_display_links = ('title', )
+
+admin.site.register(Document, DocumentAdmin)
+admin.site.register(Collection, CollectionAdmin)
