f99c3cece3a2cbf88d3af7ab3a57707e43fb396f
Niki Bizjak <drobilc@gmail.com>
2020-07-29 17:00:18 +0200

    Add login and logout view

--- LearningPlatform/settings.py
+++ LearningPlatform/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'LearningPlatform.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Where the user should be redirected after login
+LOGIN_REDIRECT_URL = '/documents'
