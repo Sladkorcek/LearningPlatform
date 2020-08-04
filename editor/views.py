2f792aff91e9d29dbefb8813f240177c1ff32ccc
Niki Bizjak <drobilc@gmail.com>
2020-08-04 11:27:40 +0200

    Add a list of starred collections and documents

--- editor/views.py
+++ editor/views.py
@@ -13,11 +13,17 @@ def help_center(request):
     return render(request, 'help_center.html')
 
 @login_required
-def documents(request):    
+def documents(request):
+
+    starred_documents = request.user.starred_documents.all()
+    starred_collections = request.user.starred_collections.all()
+
     # Show user a list of collections and documents
     return render(request, 'documents.html', {
         'collections': Collection.objects.filter(owner=request.user),
-        'documents': Document.objects.filter(owner=request.user)
+        'documents': Document.objects.filter(owner=request.user),
+        'starred_documents': starred_documents,
+        'starred_collections': starred_collections
     })
 
 def user_profile(request, user_id):
