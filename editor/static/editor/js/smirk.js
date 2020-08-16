79860a4cbfbcb37c14de24c793c313fc254313e3
Niki Bizjak <drobilc@gmail.com>
2020-08-16 11:27:44 +0200

    Update smirk library to automatically include interactive elements as needed

---
+++ editor/static/editor/js/smirk.js
@@ -0,0 +1,205 @@
+// URL's for other parts of this library
+const SMIRK_STANDARD = '/static/editor/js/smirk_standard.js';
+const SMIRK_MATH = '/static/editor/js/smirk_math.js';
+
+const INTERACTIVE_ELEMENTS_LIBRARIES = {
+    'button': SMIRK_STANDARD,
+    'flashcard': SMIRK_STANDARD,
+    'text': SMIRK_STANDARD,
+    'multipleChoice': SMIRK_STANDARD,
+    'svg': SMIRK_MATH,
+    'plot': SMIRK_MATH,
+};
+
+const REQUIREMENTS = {};
+REQUIREMENTS[SMIRK_MATH] = [
+    'https://unpkg.com/d3@3/d3.min.js',
+    'https://unpkg.com/function-plot@1/dist/function-plot.js'
+];
+
+function loadScripts(scripts, callback) {
+    if (scripts.length <= 0) {
+        callback();
+    } else {
+        let script = scripts.shift();
+        console.log('Loading library: ' + script);
+        loadScript(script, function() {
+            // The script has been loaded, load the next one
+            loadScripts(scripts, callback);
+        });
+    }
+}
+
+function loadLibrary(library, callback) {
+    let allLibraries = [ library ];
+    if (REQUIREMENTS.hasOwnProperty(library)) {
+        let libraryRequirements = REQUIREMENTS[library];
+        allLibraries = allLibraries.concat(libraryRequirements);
+    }
+    loadScripts(allLibraries, callback);
+}
+
+// window.onload executes after EVERYTHING is loaded, including images
+// The interactive blocks can start rendering before images, so
+// find a different way to detect if document has been loaded
+window.addEventListener('load', renderAll);
+
+function renderAll() {
+    
+    let interactiveBlocks = document.body.getElementsByTagName('interactive');
+    for (let i = 0; i < interactiveBlocks.length; i++) {
+        let interactiveBlock = interactiveBlocks[i];
+        renderInteractiveBlock(interactiveBlock);
+    }
+
+}
+
+function renderInteractiveBlock(interactiveBlock) {
+
+    let blockCode = interactiveBlock.innerHTML;
+
+    // If we are re-rendering elements, current interactive block contents
+    // should be removed.
+    clearInteractiveBlock(interactiveBlock);
+
+    try {
+        let render = constructRenderingFunction(blockCode);
+        let generatedElement = render();
+
+        // If the element that we want to render is an array, render all its
+        // elements in line instead of one after another
+        if (Array.isArray(generatedElement)) {
+
+            // Use Bootstrap's grid system to create a row of elements
+            let row = document.createElement("div");
+            row.className = "row";
+
+            for (let i = 0; i < generatedElement.length; i++) {
+                let column = document.createElement("div");
+                column.className = "col-sm";
+
+                try {
+                    let renderedElement = generatedElement[i].getElement(interactiveBlock.parentElement);
+                    column.appendChild(renderedElement);
+                } catch (error) {
+                    let exceptionElement = exception().withMessage(error);
+                    column.appendChild(exceptionElement.getElement());
+                }
+                row.appendChild(column);
+            }
+
+            interactiveBlock.parentElement.appendChild(row);
+
+        } else {
+            let renderedElement = generatedElement.getElement(interactiveBlock.parentElement);
+            interactiveBlock.parentElement.appendChild(renderedElement);
+        }
+        
+    } catch (error) {
+        // If the error is a ReferenceError, the user called a function that
+        // does not exist. That most likely means that a sublibrary was not
+        // loaded yet, so we will load it.
+        if (error instanceof ReferenceError) {
+            // Because JavaScript's ReferenceError object does not
+            // contain name of the variable that caused it, we will extract it from
+            // error message string.
+            let variableName = error.message.split(' ', 1)[0];
+
+            if (INTERACTIVE_ELEMENTS_LIBRARIES.hasOwnProperty(variableName)) {
+                let requiredLibrary = INTERACTIVE_ELEMENTS_LIBRARIES[variableName];
+
+                // Display loading library text
+                interactiveBlock.parentElement.appendChild(exception().withMessage("Loading smirk library").getElement());
+
+                loadLibrary(requiredLibrary, function() {
+                    // After the correct smirk library is loaded, re-render this
+                    // interactive object
+                    renderInteractiveBlock(interactiveBlock);
+                });
+            } else {
+                interactiveBlock.parentElement.appendChild(exception().withMessage(error).getElement());
+            }
+        } else {
+            interactiveBlock.parentElement.appendChild(exception().withMessage(error).getElement());
+        }
+
+    }
+
+}
+
+function constructRenderingFunction(code) {
+
+    let renderingFunctionCode = 'return (' + code + ')';
+    return new Function(renderingFunctionCode);
+
+}
+
+function clearInteractiveBlock(interactiveBlock) {
+    let parent = interactiveBlock.parentElement;
+    let children = parent.children;
+    for (let i = children.length - 1; i >= 0; i--) {
+        if (children[i].tagName.toUpperCase() !== 'INTERACTIVE') {
+            parent.removeChild(children[i]);
+        }
+    }
+}
+
+let idCounter = 1;
+
+class InteractiveElement {
+    constructor(element) {
+        this.element = element;
+        this.id = InteractiveElement.generateUniqueId();
+    }
+    getElement(parent) {
+        // This function is called with a parent element passed in as an
+        // argument. We could update our element before returning it here.
+        return this.element;
+    }
+    static generateUniqueId() {
+        idCounter += 1;
+        return idCounter - 1;
+    }
+    uniqueId() {
+        return this.id;
+    }
+}
+
+class Exception extends InteractiveElement {
+    constructor() {
+        super(document.createElement("div"));
+        this.element.className = "exception";
+    }
+    withMessage(error) {
+        this.element.innerText = error;
+        return this;
+    }
+}
+
+function exception() {
+    return new Exception();
+}
+
+function loadScript(url, callback){
+    var script = document.createElement("script")
+    script.type = "text/javascript";
+
+    if (script.readyState){  // IE
+        script.onreadystatechange = function(){
+            if (script.readyState == "loaded" ||
+                    script.readyState == "complete"){
+                script.onreadystatechange = null;
+                if (callback)
+                    callback();
+            }
+        };
+    } else {  // Others
+        script.onload = function(){
+            if (callback)
+                callback();
+        };
+    }
+
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
---
+++ editor/static/editor/js/smirk_math.js
@@ -0,0 +1,203 @@
+class Plot extends InteractiveElement {
+    constructor() {
+        let element = document.createElement("div");
+        element.className = "text-center";
+        super(element);
+
+        this.environmentData = {};
+        
+        // A list of functions that we are plotting
+        this.functions = [];
+
+        // Configuration for the function-plot library
+        this.plotData = {
+            target: this.element,
+            data: this.functions
+        };
+
+        this.additionalElements = [];
+
+        this.update();
+    }
+    getElement(parent) {
+        // If width of element is not set, use the full width of the parent
+        // element
+        if (!this.plotData.hasOwnProperty('width')) {
+            let parentWidth = parent.getBoundingClientRect().width;
+            if (parentWidth > 0) {
+                this.plotData['width'] = Math.floor(parentWidth * 0.98);
+            }
+        }
+
+        for (let i = 0; i < this.additionalElements.length; i++) {
+            this.element.appendChild(this.additionalElements[i]);
+        }
+        
+        this.update();
+        return super.getElement();
+    }
+    update() {
+        this.chart = functionPlot(this.plotData);
+        return this;
+    }
+    withGrid(displayGrid) {
+        if (displayGrid != undefined)
+            this.plotData['grid'] = displayGrid;
+        else
+            this.plotData['grid'] = true;
+        return this;
+    }
+    withDomainX(interval) {
+        this.plotData['xAxis'] = {
+            'domain': interval
+        };
+        return this;
+    }
+    withDomainY(interval) {
+        this.plotData['yAxis'] = {
+            'domain': interval
+        };
+        return this;
+    }
+    withWidth(width) {
+        this.plotData['width'] = width;
+        return this;
+    }
+    withHeight(height) {
+        this.plotData['height'] = height;
+        return this;
+    }
+    withSize(width, height) {
+        this.plotData['width'] = width;
+        this.plotData['height'] = height;
+        return this;
+    }
+    withData(dataObject) {
+        this.environmentData = dataObject;
+        return this;
+    }
+    function(functionString) {
+        if (typeof functionString === 'string' || functionString instanceof String) {
+            this.functions.push({
+                fn: functionString
+            });
+        } else {
+            this.functions.push({
+                graphType: 'polyline',
+                fn: function(scope) {
+                    return functionString(scope.x, this.environmentData);
+                }.bind(this)
+            });
+        }
+        return this;
+    }
+    uniqueSliderId() {
+        return this.additionalElements.length + 1;
+    }
+    withSliderFor(property, interval) {
+        let inputContainer = document.createElement("div");
+        inputContainer.className = "plot-slider-group";
+
+        if (!this.environmentData.hasOwnProperty(property)) {
+            this.environmentData[property] = interval[0];
+        }
+
+        let slider = document.createElement("input");
+        slider.id = "slider-" + this.uniqueSliderId();
+        slider.type = "range";
+        slider.min = interval[0];
+        slider.max = interval[1];
+        slider.step = Math.abs(interval[1] - interval[0]) / 100;
+        slider.value = this.environmentData[property];
+
+        let label = document.createElement("label");
+        label.id = "slider-label-" + this.uniqueSliderId();
+        label.htmlFor = "slider-" + this.uniqueSliderId();
+        label.innerText = "$" + property + ' = ' + slider.value + "$";
+        MathJax.typeset([label]);
+        
+        slider.addEventListener('input', function() {
+            this.environmentData[property] = Number.parseFloat(slider.value);
+            label.innerText = property + ' = ' + slider.value;
+            this.update();
+
+            if (this.latexTimer != null) {
+                clearTimeout(this.latexTimer);
+            }
+
+            // Set timeout that will re-typeset latex equations after 100ms
+            this.latexTimer = setTimeout(function() {
+                label.innerText = '$' + property + ' = ' + slider.value + '$';
+                MathJax.typeset([label]);
+                this.latexTimer = undefined;
+            }.bind(this), 100);
+
+        }.bind(this));
+
+        inputContainer.appendChild(slider);
+        inputContainer.appendChild(label);
+        this.additionalElements.push(inputContainer);
+        return this;
+    }
+}
+
+class Svg extends InteractiveElement {
+    constructor() {
+        super(document.createElement("div"));
+        this.svg = d3.select(this.element).append("svg");
+    }
+    width(width) {
+        this.svg.attr("width", width);
+        return this;
+    }
+    height(height) {
+        this.svg.attr("height", height);
+        return this;
+    }
+    applyStyling(element, styling) {
+        if (!styling)
+            return;
+        
+        for (const [key, value] of Object.entries(styling)) {
+            element.style(key, value);
+        }
+    }
+    rectangle(x, y, width, height, styling) {
+        let newElement = this.svg.append("rect")
+            .attr("x", x).attr("y", y)
+            .attr("width", width).attr("height", height);
+        this.applyStyling(newElement, styling);
+        return this;
+    }
+    ellipse(centerX, centerY, radiusX, radiusY, styling) {
+        let newElement = this.svg.append("ellipse")
+            .attr("cx", centerX)
+            .attr("cy", centerY)
+            .attr("rx", radiusX)
+            .attr("ry", radiusY);
+        this.applyStyling(newElement, styling);
+        return this;
+    }
+    circle(centerX, centerY, radius, styling) {
+        let newElement = this.svg.append("circle")
+            .attr("cx", centerX)
+            .attr("cy", centerY)
+            .attr("r", radius);
+        this.applyStyling(newElement, styling);
+        return this;
+    }
+    square(x, y, size, styling) {
+        return this.rectangle(x, y, size, size, styling);
+    }
+    point(x, y, styling) {
+        return this.circle(x, y, 3, styling);
+    }
+}
+
+function plot() {
+    return new Plot();
+}
+
+function svg() {
+    return new Svg();
+}
