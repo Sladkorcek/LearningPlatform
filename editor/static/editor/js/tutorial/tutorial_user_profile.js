00786fec31a09efeb24b999f2c7e29f6576902bb
ajdafrankovic <ajda.frankovic@gmail.com>
2020-08-22 21:27:53 +0200

    Revert "Add some more tutorial steps"
    This reverts commit 683217cb32ca374e955b1dcd80de981230ddba94.

--- editor/static/editor/js/tutorial/tutorial_user_profile.js
+++ editor/static/editor/js/tutorial/tutorial_user_profile.js
@@ -4,115 +4,115 @@
 var stepCounter = 0; // count the steps
 window.stepCounter = 0;
 
-window.addEventListener('load', function () {
-  // This function gets called when the page is loaded, everything should be
-  // initialized here  
+window.addEventListener('load', function() {
+    // This function gets called when the page is loaded, everything should be
+    // initialized here  
 
-  const driver = new Driver({
-    allowClose: false, // click on overlay should not close the tutorial
-    onHighlightStarted: function () { console.log(toString(window.stepCounter)); }
-  });
+    const driver = new Driver({
+      allowClose: false, // click on overlay should not close the tutorial
+      onHighlightStarted: function() {console.log(toString(window.stepCounter));}
+    });
 
-  if (stepCounter == 0) {
-    // This changes the link of the "new" button for creating a new document to
-    // a pre-filled document (for the sake of the tutorial)
-    let gumb = document.getElementById("new_document_button");
-    gumb.href = gumb.href + "?type=tutorial";
-  }
-
-  // Define the steps for introduction
-  driver.defineSteps([
-    {
-      element: '#tutorial_welcome', // id of a html element that we want to highlight
-      popover: {
-        className: 'colorful-next no-previous', // class of the popover html element
-        title: '<p>Welcome to <strong>stvari.si</strong>!</p>', // title of the popover
-        description: 'We\'ll quickly guide you through the basics.', // body of a popover
-        position: 'bottom', // where in relation to the hihlighted document should the popup be
-        nextBtnText: 'Let\'s go!', // text on the next button
-        closeBtnText: 'Skip', // text on the close button
-        onHighlightStarted: function () { window.stepCounter += 1; console.log(toString(window.stepCounter)); }
-      }
-    },
-    {
-      element: '#new_document_button',
-      popover: {
-        title: 'Documents',
-        description: '<p>You can write your notes into a document. Let\'s create one right now and see how this works.</p><p>Click the purple "new" button.</p>',
-        showButtons: false,
-        position: 'top'
-      }
-    },
-    {
-      element: '#document-form',
-      popover: {
-        title: 'Editor',
-        description: '<p>We\'ve prepared a sample document for you below. You can use Markdown and LateX language to style the document and Smrik to add interactive elements. More on that later. You can try editing this document now or come back later when you learn more.</p><p>When you are ready to continue the tour, click on purple "Save" button at the bottom.</p>',
-        showButtons: false,
-        position: 'top'
-      }
-    },
-    {
-      element: '#button-change-visibility',
-      popover: {
-        className: 'colorful-next no-previous no-close',
-        title: 'Visibility',
-        description: 'As you can see, this document is currently PRIVATE, which means you are the only one who can see it. You can change visibility to LINK and make it accessible to anyone with the link, or to PUBLIC so everyone can see it.',
-        position: 'bottom',
-        nextBtnText: 'Okay, go on.'
-      }
-    },
-    {
-      element: '#explore-button',
-      popover: {
-        title: 'Explore!',
-        description: 'Public documents will appear on the explore page, let\'s head there now. Click "Explore".',
-        showButtons: false,
-        position: 'bottom'
-      }
-    },
-    {
-      element: '#new_document_button',
-      popover: {
-        title: 'Documents',
-        description: 'You can  button.',
-        showButtons: false,
-        position: 'top'
-      }
-    },
-    {
-      element: '#new_document_button',
-      popover: {
-        title: 'Documents',
-        description: 'You can  button.',
-        showButtons: false,
-        position: 'top'
-      }
+    if(stepCounter == 0) {
+      // This changes the link of the "new" button for creating a new document to
+      // a pre-filled document (for the sake of the tutorial)
+      let gumb = document.getElementById("new_document_button");
+      gumb.href = gumb.href + "?type=tutorial";
     }
-  ]);
+
+    // Define the steps for introduction
+    driver.defineSteps([
+      {
+        element: '#tutorial_welcome', // id of a html element that we want to highlight
+        popover: {
+          className: 'colorful-next no-previous', // class of the popover html element
+          title: '<p>Welcome to <strong>stvari.si</strong>!</p>', // title of the popover
+          description: 'We\'ll quickly guide you through the basics.', // body of a popover
+          position: 'bottom', // where in relation to the hihlighted document should the popup be
+          nextBtnText: 'Let\'s go!', // text on the next button
+          closeBtnText: 'Skip', // text on the close button
+          onHighlightStarted: function() {window.stepCounter += 1; console.log(toString(window.stepCounter));}
+        }
+      },
+      {
+        element: '#new_document_button',
+        popover: {
+          title: 'Documents',
+          description: 'You can write your notes into a document. Let\'s create one right now and see how this works.\n Click the purple "new" button.',
+          showButtons: false,
+          position: 'top'
+        }
+      },
+      {
+        element: '#document-form',
+        popover: {
+          title: 'Editor',
+          description: '<p>We\'ve prepared a sample document for you below. You can use Markdown and LateX language to style the document and Smrik to add interactive elements. More on that later. You can try editing this document now or come back later when you learn more.</p><p>When you are ready to continue the tour, click on purple "Save" button at the bottom.</p>',
+          showButtons: false,
+          position: 'top'
+        }
+      },
+      {
+        element: '#button-change-visibility',
+        popover: {
+          className: 'colorful-next no-previous no-close',
+          title: 'Visibility',
+          description: 'As you can see, this document is currently PRIVATE, which means you are the only one who can see it. You can change visibility to LINK and make it accessible to anyone with the link, or to PUBLIC so everyone can see it. Public documents will also appear on the explore page, let\'s head there now.',
+          position: 'bottom',
+          nextBtnText: 'Explore!'
+        }
+      },
+      {
+        element: '#document-form',
+        popover: {
+          title: 'Documents',
+          description: 'You can  button.',
+          showButtons: false,
+          position: 'top'
+        }
+      },
+      {
+        element: '#new_document_button',
+        popover: {
+          title: 'Documents',
+          description: 'You can  button.',
+          showButtons: false,
+          position: 'top'
+        }
+      },
+      {
+        element: '#new_document_button',
+        popover: {
+          title: 'Documents',
+          description: 'You can  button.',
+          showButtons: false,
+          position: 'top'
+        }
+      }
+    ]);
 
 
-  driver.start(stepNumber = 0);
-  console.log('Tutorial!');
+    driver.start(stepNumber = 0);
+    console.log('Tutorial!');
 });
 
 function endTutorial(callback) {
-  // If this function is called, a request should be made to Django backend
-  // that will not load the tutorial libraries any more
+    // If this function is called, a request should be made to Django backend
+    // that will not load the tutorial libraries any more
 
-  let url = '/accounts/end_tutorial';
-  let request = new XMLHttpRequest();
-  request.open("GET", url, true);
-  request.send();
+    let url = '/accounts/end_tutorial';
+    let request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.send();
 
-  request.addEventListener('load', function (event) {
-    if (callback)
-      callback(true);
-  });
+    request.addEventListener('load', function(event) {
+        if (callback)
+            callback(true);
+    });
 
-  request.addEventListener('error', function (event) {
-    if (callback)
-      callback(false);
-  });
+    request.addEventListener('error', function(event) {
+        if (callback)
+            callback(false);
+    });
 }
 
